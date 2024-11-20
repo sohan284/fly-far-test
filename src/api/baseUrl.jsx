c27489eda4a8aa93b2286802f0e247bf32cef6c3
sr sohan <sr.sohan088@gmail.com>
2024-11-20 12:32:29 +0600

    add server

--- src/api/baseUrl.jsx
+++ src/api/baseUrl.jsx
@@ -1,4 +1,7 @@
 // local
-const baseUrl = `http://localhost:5000`;
+// const baseUrl = `http://localhost:5000`;
+
+//dev
+const baseUrl =`https://referai-server.vercel.app`;
 
 export default baseUrl;

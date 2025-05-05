5ffe984e1f9a2834947177abe1c05c33728df788
Sourov24 <sourav@flyfar.tech>
2025-05-05 12:26:55 +0600

    add typescript support to the project

---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import SearchIcon from '@mui/icons-material/Search';
+const Home = () => {
+  return (
+    <div className="max-w-[1180px] mx-auto mt-10">
+      <div>
+        <SearchIcon className="bg-white"/>
+        <h1 className="text-[14px]">প্রিয় ট্রেড পার্টনার, আমাদের সাথে হোয়াটসঅ্যাপ এ যোগাযোগ করতে উপরে থাকা হোয়াটসঅ্যাপ বাটনটিতে ক্লিক করুন, অথবা +৮৮০ ১৭৫৫ ৫৭২ ০৯৮ এবং +৮৮০ ১৭৫৫ ৫৭২</h1>
+      </div>
+      <h3>Hello</h3>
+    </div>
+  );
+};
+
+export default Home;
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
-import { StrictMode } from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App.tsx";
 import "./index.css";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
-createRoot(document.getElementById("root")).render(
+const rootElement = document.getElementById("root");
+if (!rootElement) {
+  throw new Error("Root element not found");
+}
+createRoot(rootElement).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
